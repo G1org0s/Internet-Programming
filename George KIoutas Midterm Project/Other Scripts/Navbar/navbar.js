8cdf54836d1fe57a618bec89b8986377823cc398
GKioutas <g.kioutas@acg.edu>
2026-06-14 17:37:56 +0300

    Fixed the navbar to become a hamburger menu when in smaller screens. I created it with a bootstrap function navbar-toggler.
    In each page html I added the required bootstrap class in order for the menu toggler to work.
    
    Only in the index page the command is shown in the rest of the pages, the menu toggler commands are located in the navbar.js

--- George KIoutas Midterm Project/Other Scripts/Navbar/navbar.js
+++ George KIoutas Midterm Project/Other Scripts/Navbar/navbar.js
@@ -1,18 +1,30 @@
 const navbar = document.querySelector("#navbar");
 
 navbar.innerHTML = `
-    <nav class="d-flex gap-3 flex-wrap">
-        <a href="../Home Page/index.html" class="btn btn-light">Home</a>
-        <a href="../Fishing Trips/trips.html" class="btn btn-light">Trips</a>
-        <a href="../Analytics/analytics.html" class="btn btn-light">Analytics</a>
-        <a href="../Reviews/reviews.html" class="btn btn-light">Reviews</a>
-        <a href="../About/about.html" class="btn btn-light">About</a>
-        <a href="../Contact/contact.html" class="btn btn-light">Contact</a>
-
-        <button id="darkModeToggle"
-                class="btn btn-dark"
-                type="button">
-            Dark Mode
+    <nav class="navbar navbar-expand-lg navbar-dark p-0">
+        <button class="navbar-toggler"
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#mainNavbar"
+                aria-controls="mainNavbar"
+                aria-expanded="false"
+                aria-label="Open navigation menu">
+            <span class="navbar-toggler-icon"></span>
         </button>
+
+        <div class="collapse navbar-collapse" id="mainNavbar">
+            <div class="d-grid d-lg-flex gap-2 gap-lg-3 mt-3 mt-lg-0">
+                <a href="../Home Page/index.html" class="btn btn-light">Home</a>
+                <a href="../Fishing Trips/trips.html" class="btn btn-light">Trips</a>
+                <a href="../Analytics/analytics.html" class="btn btn-light">Analytics</a>
+                <a href="../Reviews/reviews.html" class="btn btn-light">Reviews</a>
+                <a href="../About/about.html" class="btn btn-light">About</a>
+                <a href="../Contact/contact.html" class="btn btn-light">Contact</a>
+
+                <button id="darkModeToggle" class="btn btn-dark" type="button">
+                    Dark Mode
+                </button>
+            </div>
+        </div>
     </nav>
 `;
